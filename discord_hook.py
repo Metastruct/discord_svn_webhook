7cb17af73b101f4f1178e7cdf8a1520135582f27
Techbot <techbot121@gmail.com>
2018-03-28 17:39:16 +0200

    fix extension options

--- discord_hook.py
+++ discord_hook.py
@@ -70,7 +70,7 @@ for line in _changed.split('\n'):
 
 Author = svnl('author')
 Date = re.sub('(\s\(.+\))', '', svnl('date'))
-Diff = svnl('diff', '--no-diff-deleted', '-x "-w -ignore-eol-style"')[:1990]
+Diff = svnl('diff', '--no-diff-deleted', '-x -w --ignore-eol-style')[:1990]
 Repo = path.basename(_repos)
 Log = svnl('log')
 Urls = ''
